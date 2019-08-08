91a68a2a0cf8a27e3c543518145deb84113cc918
Thalison Wilker <thalison.wilker@gmail.com>
2019-08-08 20:11:11 -0300

    Saída de erro personalizada

--- classes_python.py
+++ classes_python.py
@@ -13,15 +13,19 @@ class Rectangle(object):
         return f'Rectangle with width={self.width} and height={self.height}'
 
     def __eq__(self, other):
-        return (self.width == other.width) and (self.height == other.height)
+        if isinstance(other, Rectangle):
+            return (self.width == other.width) and (self.height == other.height)
+        else:
+            raise ArithmeticError(f'{type(other)} is not a Rectangle class instance')
 
 
 rect1 = Rectangle(10, 40)
 rect2 = Rectangle(40, 10)
+
 print(rect1 == rect2)  # False
 
 # adicionando os valores do primeiro retângulo ao segundo
 rect2.width = rect1.width
 rect2.height = rect1.height
 
-print(rect1 == rect2)  # True
+print(rect1 == 4)  # True
